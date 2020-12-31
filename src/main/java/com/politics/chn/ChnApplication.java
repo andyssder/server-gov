bb4a44ce2b108ac52d1b4e01a4c351d1911d1a1c
andyssder <yuanhanghsu@gmail.com>
2020-12-31 15:55:03 +0800

    Initial commit

---
+++ src/main/java/com/politics/chn/ChnApplication.java
@@ -0,0 +1,13 @@
+package com.politics.chn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/politics/chn/ChnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politics.chn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
